2b230956a6419e1b2b31892384532b49ace47d65
annabmv <annabm.vercesi@gmail.com>
2023-11-02 15:19:25 -0300

    Initial commit

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # Airflow needs a home. `~/airflow` is the default, but you can put it
 # somewhere else if you prefer (optional)
-export AIRFLOW_HOME=./airflow-data
+export AIRFLOW_HOME=/home/abmv/airflow_tooltorial/airflow-data
 
 # Install Airflow using the constraints file
-AIRFLOW_VERSION=2.6.0
+AIRFLOW_VERSION=2.7.2
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.6
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
